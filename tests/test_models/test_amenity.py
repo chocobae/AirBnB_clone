2e1c4d4663a04a41ba1cae3bae10a87f51ab4c98
chocobae <chocobae1995@gmail.com>
2023-02-12 15:19:39 +0000

    update

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,23 +1,18 @@
-#!/usr/bin/python3
-"""Test suite for Amenity class of the models.amenity module"""
-import unittest
 
+#!/usr/bin/python3
+"""
+Test suite for amenity class
+"""
+import unittest
 from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
-class TestAmenity(unittest.TestCase):
-    """Test cases for the Amenity class"""
+class TestBaseModel(unittest.TestCase):
+    def test_str(self):
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
 
-    def setUp(self):
-        self.amenity = Amenity()
-
-    def test_amenity_is_a_subclass_of_basemodel(self):
-        self.assertTrue(issubclass(type(self.amenity), BaseModel))
-
-    def test_attr_is_a_class_attr(self):
-        self.assertTrue(hasattr(self.amenity, "name"))
-
-    def test_class_attr(self):
-        self.assertIs(type(self.amenity.name), str)
-        self.assertFalse(bool(getattr(self.amenity, "name")))
+    def test_parent(self):
+        amenity = Amenity()
+        self.assertTrue(isinstance(amenity, BaseModel))
