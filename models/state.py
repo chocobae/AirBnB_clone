f994184cb9bd2bcdfe6d23d50864622f2d31de42
chocobae <chocobae1995@gmail.com>
2023-02-12 07:04:23 +0000

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""A module containing the State model"""
+"""Module for Review class."""
+
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """Implements the State model for any state object"""
-    name = ""
+class Review(BaseModel):
+    """Class representing a Review."""
+    place_id = ""
+    user_id = ""
+    text = ""
